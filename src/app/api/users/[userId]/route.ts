0da5407b73dff2841a926d6ae095e66b70850f36
Prayugo <prayugo.setioko@allianz.co.id>
2024-06-26 17:37:23 +0700

    update get user  and delete user

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,13 +1,24 @@
 
 import { NextRequest, NextResponse } from 'next/server';
-import User from '@/models/User';
-import connectToDB from "@/app/lib/utils"
+import { User } from '@/app/lib/models';
+import { connectToDB } from "@/app/lib/utils"
 
 const LoadDB = async()=>{
     await connectToDB();
 }
 
 LoadDB()
+
+export async function GET(req: NextRequest, { params }: { params: { userId: string } }) {
+  try {
+      const {userId} = params
+      const user = await User.findById(userId);
+      return NextResponse.json({ user });
+    } catch (err) {
+      console.error(err);
+      return NextResponse.json({ message: "Failed to fetch users!" });
+    }
+}
 
 export async function DELETE(request: NextRequest, { params }: { params: { userId: string } }) {
     const id = params.userId;
@@ -24,4 +35,7 @@ export async function DELETE(request: NextRequest, { params }: { params: { userI
       console.error('Failed to delete user:', err);
       return NextResponse.json({ message: 'Failed to delete user!' }, { status: 500 });
     }
-  }
+}
+export async function UPDATE(request: NextRequest, { params }: { params: { userId: string } }) {
+
+}
